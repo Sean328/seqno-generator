41918259d7f2ca278b888c490939293a40fc4448
sean <seanlee2333@gmail.com>
2019-01-07 23:52:26 +0800

    feat 调整实体类型

--- src/main/java/com/xxl/domain/IDMeta.java
+++ src/main/java/com/xxl/domain/IDMeta.java
@@ -4,47 +4,123 @@ package com.xxl.domain;
  * @author sean
  * @date 2019/01/06/23:52
  **/
-public class IDMeta {
+public class IDMeta extends BaseDomain{
+    private static final long serialVersionUID = -1787589189969229716L;
 
-    private long versionBits;
-    private long typeBits;
-    private long machineBits;
-    private long timeBits;
-    private long sequenceBits;
+    /**
+     * 每部分所占位数
+     */
+    private byte versionBits;
+    private byte typeBits;
+    private byte machineBits;
+    private byte timeBits;
+    private byte sequenceBits;
+
+    /**
+     * 各个部分偏移位置
+     * 向左移位的规则是，某部分移位的时候，后面部分加起来需要占多少位，那么该部分就向左移多少位
+     */
+    private long versionShift;
+    private long typeShift;
+    private long machineShift;
+    private long timeShift;
+    private long sequenctShift;
+
+    public IDMeta(byte versionBits,byte typeBits,byte machineBits,byte timeBits,byte sequenceBits){
+        this.versionBits = versionBits;
+        this.typeBits = typeBits;
+        this.machineBits = machineBits;
+        this.timeBits = timeBits;
+        this.sequenceBits = sequenceBits;
+    }
 
     public long getTypeBits() {
         return typeBits;
     }
 
-    public IDMeta setTypeBits(long typeBits) {
+    public IDMeta setTypeBits(byte typeBits) {
         this.typeBits = typeBits;
         return this;
     }
 
-    public long getMachineBits() {
+    public byte getMachineBits() {
         return machineBits;
     }
 
-    public IDMeta setMachineBits(long machineBits) {
+    public IDMeta setMachineBits(byte machineBits) {
         this.machineBits = machineBits;
         return this;
     }
 
-    public long getTimeBits() {
+    public byte getTimeBits() {
         return timeBits;
     }
 
-    public IDMeta setTimeBits(long timeBits) {
+    public IDMeta setTimeBits(byte timeBits) {
         this.timeBits = timeBits;
         return this;
     }
 
-    public long getSequenceBits() {
+    public byte getSequenceBits() {
         return sequenceBits;
     }
 
-    public IDMeta setSequenceBits(long sequenceBits) {
+    public IDMeta setSequenceBits(byte sequenceBits) {
         this.sequenceBits = sequenceBits;
+        return this;
+    }
+
+    public byte getVersionBits() {
+        return versionBits;
+    }
+
+    public IDMeta setVersionBits(byte versionBits) {
+        this.versionBits = versionBits;
+        return this;
+    }
+
+    public long getVersionShift() {
+        return versionShift;
+    }
+
+    public IDMeta setVersionShift(long versionShift) {
+        this.versionShift = versionShift;
+        return this;
+    }
+
+    public long getTypeShift() {
+        return typeShift;
+    }
+
+    public IDMeta setTypeShift(long typeShift) {
+        this.typeShift = typeShift;
+        return this;
+    }
+
+    public long getMachineShift() {
+        return machineShift;
+    }
+
+    public IDMeta setMachineShift(long machineShift) {
+        this.machineShift = machineShift;
+        return this;
+    }
+
+    public long getTimeShift() {
+        return timeShift;
+    }
+
+    public IDMeta setTimeShift(long timeShift) {
+        this.timeShift = timeShift;
+        return this;
+    }
+
+    public long getSequenctShift() {
+        return sequenctShift;
+    }
+
+    public IDMeta setSequenctShift(long sequenctShift) {
+        this.sequenctShift = sequenctShift;
         return this;
     }
 }
---
+++ src/main/java/com/xxl/factory/IdMetaFactory.java
@@ -0,0 +1,17 @@
+package com.xxl.factory;
+
+import com.robert.vesta.service.bean.Id;
+import com.xxl.domain.ID;
+import com.xxl.domain.IDMeta;
+
+/**
+ * @author sean
+ * @date 2019/01/07/23:40
+ **/
+public class IdMetaFactory {
+
+    private IDMeta idMeta;
+    private ID id;
+
+
+}
